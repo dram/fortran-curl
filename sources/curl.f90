41a359c8605677e18b9af400dd467b487921e8bc
Xin Wang <dram.wang@gmail.com>
2018-04-14 12:38:46 +0800

    Use function name instead of RESULT

--- sources/curl.f90
+++ sources/curl.f90
@@ -12,28 +12,27 @@ module curl
        type(c_ptr), value :: curl
      end subroutine curl_easy_cleanup
 
-     function curl_easy_init() result(res) &
+     function curl_easy_init() &
           bind(c, name="curl_easy_init")
        use iso_c_binding, only: c_ptr
-       type(c_ptr) :: res
+       type(c_ptr) :: curl_easy_init
      end function curl_easy_init
 
-     function curl_easy_perform(handle) result(res) &
+     function curl_easy_perform(handle) &
           bind(c, name="curl_easy_perform")
        use iso_c_binding, only: c_int, c_ptr
        type(c_ptr), value :: handle
-       integer(c_int) :: res
+       integer(c_int) :: curl_easy_perform
      end function curl_easy_perform
 
      !! See: https://curl.haxx.se/docs/todo.html#Typesafe_curl_easy_setopt
-     function curl_easy_set_ptr(handle, option, param) result(res) &
+     function curl_easy_set_ptr(handle, option, param) &
           bind(c, name="curl_easy_setopt")
        use iso_c_binding, only: c_int, c_ptr
        type(c_ptr), value :: handle
        integer(c_int), value :: option
        type(c_ptr), value :: param
-       integer(c_int) :: res
+       integer(c_int) :: curl_easy_set_ptr
      end function curl_easy_set_ptr
-
   end interface
 end module curl
